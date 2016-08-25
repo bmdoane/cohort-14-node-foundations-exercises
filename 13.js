491329eb3400e7d3678080e3f0bb23794a1cee7e
Brian Doane <brianmdoane@gmail.com>
2016-08-25 15:13:23 -0500

    jumped to 11 after some more initial code

--- 13.js
+++ 13.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 "use strict";
 
+const [,, ...args] = process.argv
 const { createServer } = require('http')
 const { get } = require('request')
 const { load } = require('cheerio')
 
 const server = createServer()
+const url = `${args}`
 
-server.on('request', (req, res) => {
-  get('https://reddit.com', (err, _, body) => {
-  	const $ = load(body)
+// server.on('request', (req, res) => {
+//   get('https://reddit.com', (err, _, body) => {
+//   	const $ = load(body)
 
   	// const topStories = []
   	// for(let i = 0, titles = $('a.title'); i > titles.length; i++) {
@@ -29,15 +31,15 @@ server.on('request', (req, res) => {
   	// 	})
   	// })
 
-  	const topStories = $('a.title')
-  		.toArray()
-  		.map($)
-  		.map(el => ({
-  			title: el.text(),
-  			link: el.attr('href')   			
-  		}))
+  	// const topStories = $('a.title')
+  	// 	.toArray()
+  	// 	.map($)
+  	// 	.map(el => ({
+  	// 		title: el.text(),
+  	// 		link: el.attr('href')   			
+  	// 	}))
   	// [{ title: 'Trump did stuff today', link: 'http://cnn.com'}, {title: 'Trump did even more stuff today', link: 'http://abcnews.com'}]
-  	res.end(JSON.stringify(topStories))
+  	res.end(JSON.stringify(????))
   })
 })
 
