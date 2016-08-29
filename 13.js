c031f254e00d97b08822366d2416d3fe44143978
Brian Doane <brianmdoane@gmail.com>
2016-08-29 09:12:47 -0500

    random commit to have status clear

--- 13.js
+++ 13.js
@@ -7,39 +7,15 @@ const { get } = require('request')
 const { load } = require('cheerio')
 
 const server = createServer()
-const url = `${args}`
 
-// server.on('request', (req, res) => {
-//   get('https://reddit.com', (err, _, body) => {
-//   	const $ = load(body)
-
-  	// const topStories = []
-  	// for(let i = 0, titles = $('a.title'); i > titles.length; i++) {
-  	// 	const $title = $(titles[i])
-  	// 	topStories.push({
-  	// 		title: $title.text(),
-  	// 		link: $title.attr('href')
-  	// 	})
-  	// }
-
-  	// const topStories = []
-  	// $('a.title').each((i, title) => {
-  	// 	const $title = $(title)
-  	// 	topStories.push({
-  	// 		title: $title.text(),
-  	// 		link: $title.attr('href')  			
-  	// 	})
-  	// })
-
-  	// const topStories = $('a.title')
-  	// 	.toArray()
-  	// 	.map($)
-  	// 	.map(el => ({
-  	// 		title: el.text(),
-  	// 		link: el.attr('href')   			
-  	// 	}))
-  	// [{ title: 'Trump did stuff today', link: 'http://cnn.com'}, {title: 'Trump did even more stuff today', link: 'http://abcnews.com'}]
-  	res.end(JSON.stringify(????))
+server.on('request', (req, res) => {
+  get('https://reddit.com', (err, _, body) => {
+  	const $ = load(body)
+  	const siteTitle = $('title')
+  	// console.log("siteTitle", siteTitle);
+  	console.log("body", body);
+  
+  res.end(JSON.stringify(body))
   })
 })
 
